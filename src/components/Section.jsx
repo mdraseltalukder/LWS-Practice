6bbedc8363a9bc9b773825a17cf29bd0fd972399
Md Rasel <mdrasel551219@gmail.com>
2025-11-21 08:33:53 +0600

    add: added 3.12

--- src/components/Section.jsx
+++
@@ -1,9 +0,0 @@
-import { LevelContext } from "../context/LavelContext";
-
-export default function Section({ level, children }) {
-  return (
-    <section className="section">
-      <LevelContext value={level}>{children}</LevelContext>
-    </section>
-  );
-}
